ad59fb56f6f5bc2d3dbef6fef08a393ad85ea265
keyurc2003 <keyurchotaliya2003@gmail.com>
2023-08-30 10:22:36 +0530

    direction set

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function executeCommands(initialPosition, initialDirection, commands) {
     let position = [...initialPosition];
     let direction = initialDirection;
 
+    let prv;
     for (const command of commands) {
         if (command === 'f') {
             if (direction === 'N') position[1]++; 
@@ -13,26 +14,35 @@ function executeCommands(initialPosition, initialDirection, commands) {
             else if (direction === 'S') position[1]++;
             else if (direction === 'E') position[0]--; 
             else if (direction === 'W') position[0]++;
+            else if (direction === 'Up') position[2]--;
         } else if (command === 'r') {
             if (direction === 'N') direction = 'E';
             else if (direction === 'E') direction = 'S';
             else if (direction === 'S') direction = 'W';
             else if (direction === 'W') direction = 'N';
+            
         } else if (command === 'l') {
-            if (direction === 'N') direction = 'W';
+            if(direction === "Up" )  direction="N";
+            else if (direction === 'N') direction = 'W';
             else if (direction === 'W') direction = 'S';
             else if (direction === 'S') direction = 'E';
             else if (direction === 'E') direction = 'N';
+        
         } else if (command === 'u') {
             if (direction === 'N') direction = 'Up';
+            else if (direction === 'E') direction = 'Up';
             else if (direction === 'Up') direction = 'S';
             else if (direction === 'S') direction = 'Down';
             else if (direction === 'Down') direction = 'N';
+            prv=command;
+            // position[2]++;  //z index not changed
         } else if (command === 'd') {
             if (direction === 'N') direction = 'Down';
             else if (direction === 'Down') direction = 'S';
             else if (direction === 'S') direction = 'Up';
             else if (direction === 'Up') direction = 'N';
+            prv=command;
+            // position[2]--;  
         }
     }
     console.log(position)
